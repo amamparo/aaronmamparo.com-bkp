4afb38842ad924511e401a7617804d74215adaf6
Aaron Mamparo <aaronmamparo@gmail.com>
2023-01-09 22:43:10 -0600

    minimal

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,24 @@
 <script>
-  import {JsonView} from '@zerodevx/svelte-json-view'
-
-  const json = {
-    name: 'Aaron Mamparo',
-    location: 'Chicago, IL',
-    social: {
-      'linkedin': 'https://www.linkedin.com/in/aaronmamparo/'
-    }
+  const socials = {
+    'github': 'https://github.com/amamparo',
+    'goodreads': 'https://www.goodreads.com/user/show/104803730-aaron-mamparo',
+    'instagram': 'https://www.instagram.com/aaronmamparo/',
+    'last.fm': 'https://www.last.fm/user/amamparo',
+    'linkedin': 'https://www.linkedin.com/in/aaronmamparo/',
+    'mastodon': 'https://mstdn.social/@aaronmamparo',
+    'soundcloud': 'https://soundcloud.com/aaronmamparo'
   }
 </script>
 
-<JsonView {json}/>
+Find me:
+<ul>
+    {#each Object.entries(socials) as [title, url]}
+        <li><a href={url}>{title}</a></li>
+    {/each}
+</ul>
+
+<style>
+    li {
+        margin-bottom: .5rem;
+    }
+</style>
