4a1dc83c8ef5ae9c8bb0f2f049bf7c336e9dffaf
Aaron Mamparo <aaronmamparo@gmail.com>
2023-01-09 21:23:38 -0600

    deploy

--- aws/.eslintrc.cjs
+++ aws/.eslintrc.cjs
@@ -3,11 +3,12 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'prettier',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
   plugins: [
-    'svelte3',
+    '@typescript-eslint'
   ],
-  overrides: [{files: ['*.svelte'], processor: 'svelte3/svelte3'}],
   parserOptions: {
     sourceType: 'module',
     ecmaVersion: 2020
