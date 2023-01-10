ba4e5af3ccd0e340c74703c680a31de054bbd2f4
Aaron Mamparo <aaronmamparo@gmail.com>
2023-01-09 21:28:12 -0600

    deploy

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,9 +3,12 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'prettier',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
   plugins: [
     'svelte3',
+    '@typescript-eslint'
   ],
   overrides: [{files: ['*.svelte'], processor: 'svelte3/svelte3'}],
   parserOptions: {
